8abd90e5adce0ff12ab2298c24a4596bad251e58
PolarBowler <f.windisch97@gmail.com>
2022-02-20 18:58:06 +0100

    Finished UserInput class

--- src/MultiCalculator.java
+++ src/MultiCalculator.java
@@ -3,13 +3,30 @@ import mymethods.UserInput;
 
 public class MultiCalculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         UserInput test = new UserInput();
 
-        int intTest = test.getInt("help");
-        System.out.println(intTest);
+        byte byteTest = test.getByte("Input a number between -128 and 127 without decimal points: ");
+        System.out.println("You entered: " + byteTest);
 
-        double doubleTest = test.getDouble("help");
-        System.out.println(doubleTest);
+        short shortTest = test.getShort("Input a number between -32,768 and 32,767 without decimal points: ");
+        System.out.println("You entered: " + shortTest);
+
+        int intTest = test.getInt("Input a number between -2,147,483,648 and 2,147,483,647 without decimal points: ");
+        System.out.println("You entered: " + intTest);
+
+        long longTest = test.getLong("Input a number between -9,223,372,036,854,775,808 and 9,223,372,036,854,775,807 without decimal points: ");
+        System.out.println("You entered: " + longTest);
+
+        float floatTest = test.getFloat("Input a number with up to 6 decimal points: ");
+        System.out.println("You entered: " + floatTest);
+
+        double doubleTest = test.getDouble("Input a number with up to 15 decimal points: ");
+        System.out.println("You entered: " + doubleTest);
+
+        String stringTest = test.getString("Input a string: ");
+        System.out.println("You entered: " + stringTest);
+
+        String stringTest2 = test.getString("Input a string(do not leave empty): ", false);
+        System.out.println("You entered: " + stringTest2);
     }
 }
--- src/mymethods/UserInput.java
+++ src/mymethods/UserInput.java
@@ -1,23 +1,93 @@
 package mymethods;
+
 import java.util.Scanner;
+import java.text.*;
 
 public class UserInput {
     private Scanner sc = new Scanner(System.in);
     private Boolean error = false;
     private String invalidInput = "Invalid input, please try again!";
+    private String tempString = "";
+    private NumberFormat currentFormat = NumberFormat.getInstance();
+
+    public byte getByte(String prompt) {
+        byte result = 0;
+
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = Byte.parseByte(tempString);
+            } catch (Exception e) {
+                System.out.println(invalidInput);
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
+
+    public short getShort(String prompt) {
+        short result = 0;
+
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = Short.parseShort(tempString);
+            } catch (Exception e) {
+                System.out.println(invalidInput);
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
 
     public int getInt(String prompt) {
         int result = 0;
 
-        System.out.println(prompt);
-
         do {
+            tempString = getString(prompt);
             error = false;
             try {
-                result = sc.nextInt();
+                result = Integer.parseInt(tempString);
             } catch (Exception e) {
                 System.out.println(invalidInput);
-                sc.next();
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
+
+    public long getLong(String prompt) {
+        long result = 0L;
+
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = Long.parseLong(tempString);
+            } catch (Exception e) {
+                System.out.println(invalidInput);
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
+
+    public float getFloat(String prompt) {
+        float result = 0.0f;
+
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = currentFormat.parse(tempString).floatValue();
+            } catch (Exception e) {
+                System.out.println(invalidInput);
                 error = true;
             }
         } while (error);
@@ -26,17 +96,55 @@ public class UserInput {
     }
 
     public double getDouble(String prompt) {
-        double result = 0;
+        double result = 0.0d;
 
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = currentFormat.parse(tempString).doubleValue();
+            } catch (Exception e) {
+                System.out.println(invalidInput);
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
+
+    public boolean getBool(String prompt) {
+        boolean result = true;
+
+        do {
+            tempString = getString(prompt);
+            error = false;
+            try {
+                result = Boolean.parseBoolean(tempString);
+            } catch (Exception e) {
+                System.out.println(invalidInput);
+                error = true;
+            }
+        } while (error);
+
+        return result;
+    }
+
+    public String getString(String prompt) {
         System.out.println(prompt);
+
+        String result = sc.nextLine();
+
+        return result;
+    }
+
+    public String getString(String prompt, Boolean allowEmpty) {
+        String result = "";
 
         do {
             error = false;
-            try {
-                result = sc.nextDouble();
-            } catch (Exception e) {
+            result = getString(prompt);
+            if (result == "" && allowEmpty == false) {
                 System.out.println(invalidInput);
-                sc.next();
                 error = true;
             }
         } while (error);
